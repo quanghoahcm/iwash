2bc71bfede5b581f2d465e4f08550b4c8230f1da
quanghoahcm <quanghoahcm@gmail.com>
2018-04-20 11:34:46 +0700

    Init

---
+++ config/config.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOSTNAME,
+    dialect: "mssql",
+
+  },
+  test: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOSTNAME,
+    dialect: "mssql",
+  },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOSTNAME,
+    dialect: process.env.DB_DIALECT,
+  }
+};
